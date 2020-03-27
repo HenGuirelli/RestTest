24fdfa8b227f8bd9cfd1ed811458f582b617c255
guirelli <henrique.guirelli@intelitrader.com.br>
2020-03-26 23:32:53 -0300

    Cria tests para caso de uso

---
+++ RestTest/RestTest.Tests/CasoDeUsoTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestTest.Tests
+{
+
+    [TestClass]
+    public class CasoDeUsoTest
+    {
+        [TestMethod]
+        public void OnErrorMessage()
+        {
+            var useCase = new UseCase(new { jsonKey1 =  "jsonValue1", jsonKey2 = 3 }, new { Success = true });
+            Assert.IsTrue(useCase.CompareResponse(new { Success = true }));
+            Assert.IsFalse(useCase.CompareResponse(new { Success = "other value" }));
+        }
+    }
+}
