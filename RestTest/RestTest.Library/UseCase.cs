4d25d43c2fe9c23deb6a29386e014d1dafae7043
guirelli <henrique.guirelli@intelitrader.com.br>
2020-03-26 23:53:18 -0300

    Cria mais testes

--- RestTest/RestTest.Library/UseCase.cs
+++ RestTest/RestTest.Library/UseCase.cs
@@ -1,16 +1,21 @@
-﻿using System;
-
-namespace RestTest.Library
+﻿namespace RestTest.Library
 {
     public class UseCase
     {
         public object Request { get; private set; }
         public object Response { get; private set; }
+        public string ErrorMessage { get; private set; }
 
         public UseCase(object request, object response)
         {
             Request = request;
             Response = response;
+        }
+
+        public UseCase(object request, object response, string errorMessage)
+            : this (request, response)
+        {
+            ErrorMessage = errorMessage ?? string.Empty;
         }
 
         public bool CompareResponse (object otherResponse)
--- RestTest/RestTest.Tests/CasoDeUsoTest.cs
+++ RestTest/RestTest.Tests/CasoDeUsoTest.cs
@@ -8,11 +8,54 @@ namespace RestTest.Tests
     public class CasoDeUsoTest
     {
         [TestMethod]
-        public void OnErrorMessage()
+        public void OnCompareResponse_ShouldCompareTypes()
         {
-            var useCase = new UseCase(new { jsonKey1 =  "jsonValue1", jsonKey2 = 3 }, new { Success = true });
+            var useCase = new UseCase(null, new { Success = true });
             Assert.IsTrue(useCase.CompareResponse(new { Success = true }));
-            Assert.IsFalse(useCase.CompareResponse(new { Success = "other value" }));
+            Assert.IsFalse(useCase.CompareResponse(new { Success = 1 }));
+            Assert.IsFalse(useCase.CompareResponse(new { Success = 1m }));
+            Assert.IsFalse(useCase.CompareResponse(new { Success = "non empty string" }));
+        }
+
+        [TestMethod]
+        public void OnCompareResponse_ShouldCompareComplexValues()
+        {
+            var useCase1 = new UseCase(null, new { Int = 1 });
+            Assert.IsTrue(useCase1.CompareResponse(new { Int = 1 }));
+            Assert.IsFalse(useCase1.CompareResponse(new { Int = 2 }));
+            Assert.IsFalse(useCase1.CompareResponse(new { Int = -1 }));
+            Assert.IsFalse(useCase1.CompareResponse(new { Int = int.MaxValue }));
+            
+            var useCase2 = new UseCase(null, new { Bool = true });
+            Assert.IsTrue(useCase2.CompareResponse(new { Bool = true }));
+            Assert.IsFalse(useCase2.CompareResponse(new { Bool = false }));
+        }
+
+        [TestMethod]
+        public void OnCompareResponse_ShouldComparePrimitiveValues()
+        {
+            var useCase1 = new UseCase(null, "raw text");
+            Assert.IsTrue(useCase1.CompareResponse("raw text"));
+            Assert.IsFalse(useCase1.CompareResponse("other raw text"));
+            Assert.IsFalse(useCase1.CompareResponse(10));
+            Assert.IsFalse(useCase1.CompareResponse(true));
+
+            var useCase2 = new UseCase(null, 10);
+            Assert.IsTrue(useCase2.CompareResponse(10));
+            Assert.IsFalse(useCase2.CompareResponse(0));
+            Assert.IsFalse(useCase2.CompareResponse(-1));
+            Assert.IsFalse(useCase2.CompareResponse(1));
+        }
+
+        [TestMethod]
+        public void OnCompareResponse_NotHaveResponse()
+        {
+            var useCase1 = new UseCase(null, "raw text");
+            Assert.IsFalse(useCase1.CompareResponse(null));
+
+            var useCase2 = new UseCase(null, null);
+            Assert.IsTrue(useCase2.CompareResponse(null));
+            Assert.IsFalse(useCase2.CompareResponse("any response wrong"));
         }
     }
 }
