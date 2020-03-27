943559aa300a2520b972ea18f405baead355be77
guirelli <henrique.guirelli@intelitrader.com.br>
2020-03-26 23:41:58 -0300

    Bug fix
    Altera dynamic para object

---
+++ RestTest/RestTest.Library/UseCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RestTest.Library
+{
+    public class UseCase
+    {
+        public object Request { get; private set; }
+        public object Response { get; private set; }
+
+        public UseCase(object request, object response)
+        {
+            Request = request;
+            Response = response;
+        }
+
+        public bool CompareResponse (object otherResponse)
+        {
+            return object.Equals(Response, otherResponse);
+        }
+    }
+}
