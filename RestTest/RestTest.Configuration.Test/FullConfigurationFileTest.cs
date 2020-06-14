63d3b9426ae0275d54e4902a5676156ced424957
guirelli <henrique.guirelli@intelitrader.com.br>
2020-06-14 18:47:08 -0300

    Add full config test

---
+++ RestTest/RestTest.Configuration.Test/FullConfigurationFileTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RestTest.Configuration.Test
+{
+    [TestClass]
+    public class FullConfigurationFileTest
+    {
+        [TestMethod]
+        public void OnReadFullConfig()
+        {
+            // Not should throw exception
+            var conf = new Configuration("./full_config.json");
+
+            Assert.AreEqual(1, conf.Sequences.Count());
+            Assert.AreEqual(3, conf.Sequences.Single().Sequence.Count());
+
+            Assert.AreEqual(1, conf.Uniques.Count());
+        }
+    }
+}
