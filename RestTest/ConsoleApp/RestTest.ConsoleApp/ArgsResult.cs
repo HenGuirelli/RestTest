5f7fa3111c3c3137857ea54d833f97bd3e05461e
guirelli <henrique.guirelli@intelitrader.com.br>
2020-06-26 21:22:34 -0300

    Create console app to use RestTest

---
+++ RestTest/ConsoleApp/RestTest.ConsoleApp/ArgsResult.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace RestTest.ConsoleApp
+{
+    internal class ArgsResult
+    {
+        private readonly List<string> _helpOptions = new List<string>() { "help", "-help", "-h", "" };
+        public bool IsHelp { get; private set; }
+        public string ConfigPath { get; private set; }
+        public string ResultPath { get; private set; }
+
+        public ArgsResult(string[] args)
+        {
+            if (args.Length == 0 ||
+                (args.Length == 1 && _helpOptions.Contains(args[0])))
+            {
+                IsHelp = true;
+                return;
+            }
+
+            if (args.Length == 2)
+            {
+                ConfigPath = args[0];
+                ResultPath = args[1];
+                return;
+            }
+
+            IsHelp = true;
+        }
+    }
+}
---
+++ RestTest/ConsoleApp/RestTest.ConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using RestTest.Library;
+using System;
+using System.IO;
+
+namespace RestTest.ConsoleApp
+{
+    class Program
+    {
+        static string _resultPath;
+
+        static void Main(string[] args)
+        {
+            ArgsResult argsResult = ParseArgs(args);
+
+            if (argsResult.IsHelp)
+            {
+                Console.WriteLine("Help:");
+                Console.WriteLine("Use 2 arguments. First argument is path of config file, second argument is path of result test");
+                Console.WriteLine("Example:");
+                Console.WriteLine("\tRestTest.ConsoleApp C:\\\\folder\\config.json .\\result.json");
+
+                Console.ReadKey();
+                return;
+            }
+
+            _resultPath = argsResult.ResultPath;
+            RT rt = default;
+            try
+            {
+
+                rt = new RT(argsResult.ConfigPath);
+                rt.OnTestFinished += OnTestFinished;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private static ArgsResult ParseArgs(string[] args)
+        {
+            return new ArgsResult(args);
+        }
+
+        private static void OnTestFinished(TestResult result)
+        {
+            if (result.Status == Status.Ok)
+            {
+                File.AppendAllText(_resultPath, $"{result.TestName}: {result.Status}");
+            }
+            else
+            {
+                File.AppendAllText(_resultPath, $"{result.TestName}: {result.Status} {result.Error}");
+            }
+        }
+    }
+}
