d31e5e78bf469b62750abc36ddabbb93379a771b
guirelli <henrique.guirelli@intelitrader.com.br>
2020-07-03 18:32:58 -0300

    Create Crud Asp net endpoints

---
+++ RestTest/RestTest.AspNet/Controllers/UserCrudController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestTest.AspNet.Controllers
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserCrudController : ControllerBase
+    {
+        private static ConcurrentDictionary<Guid, User> _db = new ConcurrentDictionary<Guid, User>();
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            return Ok(new { user = _db[id] });
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            var id = Guid.NewGuid();
+            _db[id] = user;
+            return Ok(new { user_id = user });
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            _db.TryRemove(id, out var _);
+        }
+    }
+}
