94b046e5f10b927332566440b724e836a5792f31
guirelli <henrique.guirelli@intelitrader.com.br>
2020-07-08 01:42:43 -0300

    Separe class in files

---
+++ RestTest/RestTest.Library.UnitTest/ClassTest.cs
@@ -0,0 +1,18 @@
+﻿using RestTest.RestRequest;
+using System.Collections.Concurrent;
+using RestTestResult = RestTest.Library.Entity.Test.TestResult;
+
+namespace RestTest.Library.Test
+{
+    internal class ClassTest
+    {
+        public Requests Request { get; set; }
+        public ConcurrentDictionary<string, RestTestResult> Results { get; private set; }
+            = new ConcurrentDictionary<string, RestTestResult>();
+
+        public void OnFinished(RestTestResult testResult)
+        {
+            Results[testResult.TestName] = testResult;
+        }
+    }
+}
--- RestTest/RestTest.Library.UnitTest/ValidateTests.cs
+++ RestTest/RestTest.Library.UnitTest/ValidateTests.cs
@@ -1,27 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestTest.HttpServer.Test;
-using RestTest.RestRequest;
-using System.Collections.Concurrent;
 using System.Text.RegularExpressions;
-using System.Threading;
-using RestTest.Library.Entity;
-using RestTestResult = RestTest.Library.Entity.Test.TestResult;
 using RestTest.Library.Entity.Test;
 
 namespace RestTest.Library.Test
 {
-    internal class ClassTest
-    {
-        public Requests Request { get; set; }
-        public ConcurrentDictionary<string, RestTestResult> Results { get; private set; }
-            = new ConcurrentDictionary<string, RestTestResult>();
-
-        public void OnFinished(RestTestResult testResult)
-        {
-            Results[testResult.TestName] = testResult;
-        }
-    }
-
     [TestClass]
     public class ValidateTests
     {
